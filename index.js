50367cd88f61dbb68f0188fcc9ccbb6b433cf388
Maher Aboulhosn <maherhosn@gmail.com>
2024-10-14 09:25:54 -0400

    semantic fix for the questions[] array

--- index.js
+++ index.js
@@ -5,15 +5,53 @@ import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the Title of your project?",
-    "Can you provide a description for this project?",
-    "What are the steps required to install your project?",
-    "Can you provide instructions and examples for use?",
-    "Choose a license from the following options:",
-    "Would you like other devlopers to contribute to this application?",
-    "What are the steps to test this application?",
-    "What is your github username?",
-    "What is your email address?"
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the Title of your project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Can you provide a description for this project?"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Can you provide instructions and examples for use?"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose a license from the following options:",
+        choices: ["Apache", "Boost", "MIT","None"]
+    },
+    {
+        type: 'checkbox',
+        name: 'contributing',
+        message: "Would you like other devlopers to contribute to this application?",
+        choices: ["yes", "no"]
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "What are the steps to test this application?"
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your github username?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+    }
 ];
 
 // TODO: Create a function to write README file
@@ -25,57 +63,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: questions[0],
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: questions[1],
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: questions[2]
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: questions[3]
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: questions[4],
-                choices: ["Apache", "Boost", "MIT","None"]
-            },
-            {
-                type: 'checkbox',
-                name: 'contributing',
-                message: questions[5],
-                choices: ["yes", "no"]
-            },
-            {
-                type: 'input',
-                name: 'tests',
-                message: questions[6],
-            },
-            {
-                type: 'input',
-                name: 'username',
-                message: questions[7],
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: questions[8],
-            },
-        ])
-        .then((answers) => {
+    inquirer.prompt(questions).then((answers) => {
             const readMeFile = generateMarkdown(answers);
             writeToFile("README.md", readMeFile);
         });
